7b1fd91a62dc4368c9ec1d9fdb016e33c7dfe5fe
Aziz <azizvbi33@gmail.com>
2022-03-17 06:58:35 +0700

    Banyuwangi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:submarine/detail_screen.dart';
+import 'package:wonderfulbanyuwangi/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Submarine',
+      title: 'Wonderful Banyuwangi',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:submarine/main.dart';
+import 'package:wonderfulbanyuwangi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
